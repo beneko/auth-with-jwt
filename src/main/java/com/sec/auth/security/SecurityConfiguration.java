bb423b219b6e5aa0bb0fd48fe6b62b40bb3c1076
Behzad <behzadnekouei@gmail.com>
2022-12-13 12:36:54 +0100

    change security conf => csrf and frames disabled

--- src/main/java/com/sec/auth/security/SecurityConfiguration.java
+++ src/main/java/com/sec/auth/security/SecurityConfiguration.java
@@ -13,11 +13,12 @@ public class SecurityConfiguration  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+        http.authorizeHttpRequests(
+                (auth) -> auth.anyRequest().authenticated() ).httpBasic(withDefaults()
+        );
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
         return http.build();
     }
 
